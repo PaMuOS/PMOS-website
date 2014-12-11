4b9c0eea0e970b38dabcc8a81b761b69f74b1131
Sébastien Piquemal <sebpiq@gmail.com>
2014-12-11 09:17:35 +0200

    fix db

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@ var path = require('path')
 
 module.exports = {
   db: {
-    //url: 'mongodb://nodejitsu:92fc2d2c3a349abc3a2d340def38a866@troup.mongohq.com:10064/nodejitsudb5682505848'
-    url: 'mongodb://localhost:27017/pmos-test'
+    url: 'mongodb://nodejitsu:92fc2d2c3a349abc3a2d340def38a866@troup.mongohq.com:10064/nodejitsudb5682505848'
+    //url: 'mongodb://localhost:27017/pmos-test'
   },
   web: {
     hostname: 'pmos-website.jit.su',
